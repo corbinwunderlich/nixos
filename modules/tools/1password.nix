7cd6e86547d967a979fdf5d862b707a51861593e
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-02-19 20:26:01 -0500

    Only scale 1Password if the WM is Hyprland

--- modules/tools/1password.nix
+++ modules/tools/1password.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  pkgs,
+  machine,
   ...
 }: {
   options.passwordmanager.enable = lib.mkEnableOption "Enables 1password";
@@ -10,11 +10,14 @@
     nixpkgs.overlays = [
       (final: prev: {
         _1password-gui = prev._1password-gui.overrideAttrs (_old: {
-          postFixup = ''
-            wrapProgram $out/bin/1password \
-              --set ELECTRON_OZONE_PLATFORM_HINT x11 \
-              --set GDK_DPI_SCALE 1.5
-          '';
+          postFixup =
+            if config.programs.hyprland.enable
+            then ''
+              wrapProgram $out/bin/1password \
+                --set ELECTRON_OZONE_PLATFORM_HINT x11 \
+                --set GDK_DPI_SCALE 1.5
+            ''
+            else "";
         });
       })
     ];
