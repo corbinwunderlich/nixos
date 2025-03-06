1374a03fdaceb51152c506ef54730f5f3611df47
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-03-06 18:20:04 -0500

    Move input device configuration to /modules

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 {
   config,
   lib,
@@ -26,19 +23,6 @@
   services.udev.extraRules = ''
     ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x15b8" ATTR{power/wakeup}="disabled"
   '';
-
-  services.udev.packages = [
-    (pkgs.writeTextFile {
-      name = "sayo-rules";
-
-      text = ''
-        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="8089", TAG+="uaccess"
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="8089", TAG+="uaccess"
-      '';
-
-      destination = "/etc/udev/rules.d/70-sayo.rules";
-    })
-  ];
 
   services.btrfs.autoScrub.enable = true;
 
@@ -66,24 +50,6 @@
   users.users.corbin = import ./../users/corbin/corbin.nix;
   users.defaultUserShell = pkgs.zsh;
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
   environment.systemPackages = [
     inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default
     pkgs.rose-pine-cursor
@@ -98,21 +64,6 @@
 
   programs.coolercontrol.enable = true;
 
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   services.openssh = {
     enable = true;
 
@@ -121,33 +72,5 @@
 
   services.fstrim.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
---
+++ modules/tools/input-devices.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.sayodevice.enable = lib.mkEnableOption "Enable configuration for the Sayodevice O3C v1";
+  options.neo65.enable = lib.mkEnableOption "Enable the FN key for the Neo65";
+
+  config.services.udev.packages = lib.mkIf config.sayodevice.enable [
+    (pkgs.writeTextFile {
+      name = "sayo-rules";
+
+      text = ''
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="8089", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="8089", TAG+="uaccess"
+      '';
+
+      destination = "/etc/udev/rules.d/70-sayo.rules";
+    })
+  ];
+
+  config.boot.extraModprobeConfig = ''
+    options hid_apple fnmode=2
+  '';
+}
