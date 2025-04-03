ec8c2a795e184375e08e893f0b308fd545a399ac
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-04-03 16:47:25 -0400

    Add btop

--- modules/common.nix
+++ modules/common.nix
@@ -5,7 +5,8 @@
 }: {
   environment.shells = with pkgs; [zsh];
   programs.zsh.enable = true;
-  environment.systemPackages = with pkgs; [wget inputs.nixvim.packages.x86_64-linux.default git lazygit htop];
+  environment.systemPackages = with pkgs; [wget inputs.nixvim.packages.x86_64-linux.default git lazygit htop btop];
   programs.nix-ld.enable = true;
+
   nix.settings.trusted-users = ["@wheel root"];
 }
