7c6baec18b0f38f1f2ed41d072d017a4164970f0
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-04-29 12:21:32 -0400

    Add stm32pio

--- modules/tools/arduino.nix
+++ modules/tools/arduino.nix
@@ -7,6 +7,24 @@
   options.arduino.enable = lib.mkEnableOption "Enables arduino tools and kicad";
 
   config = lib.mkIf config.arduino.enable {
-    environment.systemPackages = with pkgs; [arduino-cli arduino-language-server kicad];
+    environment.systemPackages = with pkgs; [
+      arduino-cli
+      arduino-language-server
+      kicad
+
+      (pkgs.python313Packages.buildPythonPackage rec {
+        pyproject = true;
+
+        pname = "stm32pio";
+        version = "2.1.2";
+
+        src = pkgs.fetchPypi {
+          inherit pname version;
+          hash = "sha256-jiYWV9UMERWCUrBmnFPyoojCnD/7LTNcT3zavVECb+8=";
+        };
+
+        nativeBuildInputs = with pkgs.python313Packages; [setuptools setuptools-scm];
+      })
+    ];
   };
 }
