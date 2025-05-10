e75f7c76bbe12eb078ba640bec9cd4c40225455d
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-05-09 22:07:56 -0400

    Switch Bambu Studio out for Orca Slicer

--- modules/apps/3d-printing.nix
+++ modules/apps/3d-printing.nix
@@ -7,6 +7,18 @@
   options.bambu-studio.enable = lib.mkEnableOption "Enables Bambu Studio slicer and other 3d printing utilities";
 
   config = lib.mkIf config.bambu-studio.enable {
-    environment.systemPackages = with pkgs; [bambu-studio];
+    environment.systemPackages = with pkgs; [
+      (runCommand "orca-slicer" {nativeBuildInputs = [pkgs.makeWrapper];} ''
+        mkdir -p $out/bin
+
+        cp ${pkgs.orca-slicer}/bin/orca-slicer $out/bin/orca-slicer
+
+        wrapProgram $out/bin/orca-slicer --set GDK_DPI_SCALE 1.25
+
+        mkdir -p $out/share
+
+        cp -r ${pkgs.orca-slicer}/share/* $out/share
+      '')
+    ];
   };
 }
