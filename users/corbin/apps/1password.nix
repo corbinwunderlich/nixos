8a0c9c16f2f9123a0e8fdfbe5ee8653a4f87d4f5
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-05-09 22:08:52 -0400

    Add some more SSH config

--- users/corbin/apps/1password.nix
+++ users/corbin/apps/1password.nix
@@ -9,9 +9,37 @@
   config = lib.mkIf config.passwordmanager.enable {
     programs.ssh = {
       enable = true;
+
       extraConfig = ''
         IdentityAgent ~/.1password/agent.sock
+
+        Match Originalhost siarnaq Exec "host siarnaq.ridgewood"
+          HostName siarnaq.ridgewood
+          ProxyJump none
+        Host siarnaq
+          User corbin
+          HostName ridgewood.wcopy.net
+          ProxyJump ampere
+          SetEnv TERM=xterm-256color
       '';
+
+      matchBlocks = {
+        "instance-1.wcopy.net" = {
+          hostname = "instance-1.wcopy.net";
+          user = "opc";
+        };
+
+        "tethys" = {
+          hostname = "tethys.ridgewood";
+          user = "root";
+        };
+
+        "nixpi*" = {
+          user = "root";
+        };
+      };
+
+      forwardAgent = true;
     };
 
     xdg.configFile."1Password/ssh/agent.toml".text = ''
