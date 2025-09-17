1bb6e25382ef9a0bae58df1659c877d490c65c8f
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-09-17 18:45:55 -0400

    Add obsidian to flatpak

--- modules/apps/obsidian.nix
+++ modules/apps/obsidian.nix
@@ -7,6 +7,10 @@
   options.obsidian.enable = lib.mkEnableOption "Enables Obsidian.md";
 
   config = lib.mkIf config.obsidian.enable {
-    environment.systemPackages = with pkgs; [obsidian];
+    services.flatpak.enable = true;
+
+    services.flatpak.packages = [
+      "md.obsidian.Obsidian"
+    ];
   };
 }
