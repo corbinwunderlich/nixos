3de4565325deb24f8fd75001a9bc2692dc8ead89
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-09-17 14:42:39 -0400

    Switch BambuStudio to nix-flatpak

--- modules/apps/3d-printing.nix
+++ modules/apps/3d-printing.nix
@@ -7,18 +7,10 @@
   options.bambu-studio.enable = lib.mkEnableOption "Enables Bambu Studio slicer and other 3d printing utilities";
 
   config = lib.mkIf config.bambu-studio.enable {
-    environment.systemPackages = with pkgs; [
-      (runCommand "orca-slicer" {nativeBuildInputs = [pkgs.makeWrapper];} ''
-        mkdir -p $out/bin
+    services.flatpak.enable = true;
 
-        cp ${pkgs.orca-slicer}/bin/orca-slicer $out/bin/orca-slicer
-
-        wrapProgram $out/bin/orca-slicer --set GDK_DPI_SCALE 1.25
-
-        mkdir -p $out/share
-
-        cp -r ${pkgs.orca-slicer}/share/* $out/share
-      '')
+    services.flatpak.packages = [
+      "com.bambulab.BambuStudio"
     ];
   };
 }
