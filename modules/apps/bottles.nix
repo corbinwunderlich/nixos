34c79c5a4760840240907fc63c5a62551eab3ad6
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-09-17 18:46:02 -0400

    Add bottles to flatpak

--- modules/apps/bottles.nix
+++ modules/apps/bottles.nix
@@ -1,12 +1,15 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: {
   options.bottles.enable = lib.mkEnableOption "Enables Bottles";
 
   config = lib.mkIf config.bottles.enable {
-    environment.systemPackages = with pkgs; [bottles];
+    services.flatpak.enable = true;
+
+    services.flatpak.packages = [
+      "com.usebottles.bottles"
+    ];
   };
 }
