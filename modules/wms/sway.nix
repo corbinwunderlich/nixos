5f6f82374b2eb5f0e914a4bea6ec51205ef4ab2d
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-09-21 20:02:24 -0400

    Enable ozone wayland

--- modules/wms/sway.nix
+++ modules/wms/sway.nix
@@ -28,6 +28,8 @@
 
       services.gnome.gnome-keyring.enable = true;
 
+      environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
       programs.uwsm = {
         enable = true;
         waylandCompositors = {
