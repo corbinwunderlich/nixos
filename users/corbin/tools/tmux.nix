420bc78a7c1c3de165cf0fb8c190f54fcd60bdaa
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-10-18 18:58:37 -0400

    Change tmux status line

--- users/corbin/tools/tmux.nix
+++ users/corbin/tools/tmux.nix
@@ -20,22 +20,21 @@
 
       mouse = true;
 
-      plugins = with pkgs.tmuxPlugins;
-        [
-          sensible
-        ]
-        ++ (with pkgs; [
-          {
-            plugin = tmuxPlugins.resurrect;
-            extraConfig = ''
-              resurrect_dir="$HOME/.tmux/resurrect"
-              set -g @resurrect-dir $resurrect_dir
-              set -g @resurrect-hook-post-save-all "sed -i 's/--cmd lua.*--cmd set packpath/--cmd \"lua/g; s/--cmd set rtp.*\$/\"/' $resurrect_dir/last"
-              set -g @resurrect-capture-pane-contents 'on'
-              set -g @resurrect-processes '"~nvim"'
-            '';
-          }
-        ]);
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        better-mouse-mode
+
+        {
+          plugin = resurrect;
+          extraConfig = ''
+            resurrect_dir="$HOME/.tmux/resurrect"
+            set -g @resurrect-dir $resurrect_dir
+            set -g @resurrect-hook-post-save-all "sed -i 's/--cmd lua.*--cmd set packpath/--cmd \"lua/g; s/--cmd set rtp.*\$/\"/' $resurrect_dir/last"
+            set -g @resurrect-capture-pane-contents 'on'
+            set -g @resurrect-processes '"~nvim"'
+          '';
+        }
+      ];
 
       extraConfig = ''
         set -g base-index 1
@@ -51,6 +50,15 @@
 
         set -g status-bg "#000000"
         set -g status-fg "#ffffff"
+
+        set -g status-right "@#H - #(pwd)"
+        set -g status-left "#S"
+        set -g status-left-length 100
+        set -g status-justify centre
+        set -g window-status-format "#[fg=#ffffff]#I: #W"
+        set -g window-status-current-format "#[fg=blue,bold]#I: #W"
+
+        set-option -g status-position top
 
         bind-key "T" run-shell "sesh connect \"$(
           sesh list --icons | fzf-tmux -p 80%,70% \
