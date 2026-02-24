66d8e64b8c10bb5d96f7318e2e2a6c477153bf07
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2026-02-23 22:04:45 -0500

    Delete opencode

--- users/corbin/apps/neovim.nix
+++ users/corbin/apps/neovim.nix
@@ -25,68 +25,6 @@
       };
     };
 
-    systemd.user.services."opencode-config" = {
-      Service = {
-        WorkingDirectory = "/home/corbin";
-
-        Type = "oneshot";
-
-        Environment = "PATH=${pkgs.coreutils-full}/bin";
-
-        ExecStart = let
-          script = pkgs.writeShellScriptBin "opencode-config" ''
-            mkdir -p /home/corbin/.config/opencode
-
-            if [ -f "/home/corbin/.config/opencode/opencode.json" ]; then rm /home/corbin/.config/opencode/opencode.json; fi
-
-            url="$(cat ${config.sops.secrets."ollama/url".path})"
-            key="$(cat ${config.sops.secrets."ollama/key".path})"
-
-            cat <<EOL >> /home/corbin/.config/opencode/opencode.json
-            {
-              "provider": {
-                "ollama": {
-                  "npm": "@ai-sdk/openai-compatible",
-                  "name": "Ollama",
-                  "options": {
-                    "baseURL": "$url/v1",
-                    "headers": {
-                      "Authorization": "Bearer $key"
-                    }
-                  },
-                  "models": {
-                    "gpt-oss:20b": {
-                      "name": "GPT OSS 20b"
-                    },
-                    "qwen3-coder:30b": {
-                      "name": "Qwen Coder 30b"
-                    }
-                  }
-                }
-              }
-            }
-            EOL
-          '';
-        in "${script}/bin/opencode-config";
-      };
-
-      Install = {
-        WantedBy = ["default.target"];
-      };
-    };
-
-    programs.bash.bashrcExtra = ''
-      export OLLAMA_API_KEY="$(cat ${config.sops.secrets."ollama/key".path})"
-    '';
-
-    programs.zsh.initContent = ''
-      export OLLAMA_API_KEY="$(cat ${config.sops.secrets."ollama/key".path})"
-    '';
-
-    programs.nushell.extraEnv = ''
-      $env.OLLAMA_API_KEY = (cat ${config.sops.secrets."ollama/key".path})
-    '';
-
     home.sessionVariables = {
       EDITOR = "nvim";
       SUDO_EDITOR = "nvim";
@@ -97,7 +35,6 @@
       ripgrep
       fzf
       inputs.nixvim.packages.x86_64-linux.default
-      opencode
     ];
   };
 }
