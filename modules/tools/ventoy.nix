cd8664a56b63a88f2a87e7e1eed72a9877df85e6
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2026-03-17 19:32:08 -0400

    Update flake.lock

--- modules/tools/ventoy.nix
+++ modules/tools/ventoy.nix
@@ -9,7 +9,9 @@
   config = lib.mkIf config.ventoy.enable {
     nixpkgs.config.permittedInsecurePackages = [
       "ventoy-1.1.07"
+      "ventoy-1.1.10"
       "ventoy-gtk3-1.1.07"
+      "ventoy-gtk3-1.1.10"
     ];
 
     environment.systemPackages = with pkgs; [ventoy ventoy-full-gtk caligula];
